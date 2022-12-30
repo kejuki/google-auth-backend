06b46990b6fcc8a175a341393e331708af67849a
kejuki <kerkkokiikeri97@gmail.com>
2022-12-30 16:43:24 +0200

    clean up

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require("express")
 const app = express()
 const cors = require('cors')
-const session = require('express-session');
 const routes = require('./routes/routes')
 
 app.use(cors({origin: "http://localhost:3000", credentials:true}))
 app.use(express.json())
 app.use('/api', routes)
-
-const users = [] 
 
 app.listen(process.env.PORT || 3001 , console.log(`runnin on port ${process.env.PORT}`))
